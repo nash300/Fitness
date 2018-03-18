f7589eedc86e2695bfd58cb02cfc81b5c9d8d9d5
Nadeesha <nash@Nadeeshas-iMac.lan>
2018-03-18 19:37:18 +0100

    initialized BMI calculator

--- myscript.js
+++ myscript.js
@@ -1,1 +1,35 @@
 
+window.onload = metricRadio;
+
+
+function metricRadio() {  // hides input & lable for "inches".
+
+var h_unit1 = document.getElementById("h_unit1");
+var w_unit = document.getElementById("w_unit");
+h_unit1.innerHTML = "cm";
+w_unit.innerHTML = "kg";
+
+var item1 = document.getElementById("height2");
+var item2 = document.getElementById("h_unit2");
+
+item1.style.visibility = "hidden";
+item2.style.visibility = "hidden";
+}
+
+
+function standard() { // adds feets & inches to Height input 
+
+    var item1 = document.getElementById("height2");
+    var item2 = document.getElementById("h_unit2");
+    
+    item1.style.visibility = "visible";
+    item2.style.visibility = "visible";
+
+    var h_unit1 = document.getElementById("h_unit1");
+    var w_unit = document.getElementById("w_unit");
+    var h_unit2 = document.getElementById("h_unit2");
+    
+    h_unit1.innerHTML = "feets";
+    w_unit.innerHTML = "pounds";
+    h_unit2.innerHTML = "inches";
+}
