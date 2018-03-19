ccebc93826b5c87212d83aebe8e54c3ca1233761
Nadeesha <nash@Nadeeshas-iMac.lan>
2018-03-19 22:52:42 +0100

    BMI calculator

--- myscript.js
+++ myscript.js
@@ -33,3 +33,53 @@ function standard() { // adds feets & inches to Height input
     w_unit.innerHTML = "pounds";
     h_unit2.innerHTML = "inches";
 }
+
+
+
+
+
+function cal() {  // this function determines which function needs to be nun according to unit type.
+
+var metric = document.getElementById("radio_metric").checked;
+var standard = document.getElementById("radio_standard").checked;
+
+    if (metric = "true") {
+        cal_metric();
+    }
+
+    if (standard = "true") {
+        cal_standard();
+    }
+    }
+
+
+function cal_metric() {  // counts BMI for metric units.
+    var cm = document.getElementById("height1").value/100;
+    var kg = document.getElementById("weight").value;
+
+    var answer = kg/(cm*cm);
+
+    var bmi = document.getElementById("bmi");
+
+    bmi.innerHTML = answer.toFixed(1);
+}
+
+
+
+
+
+function cal_standard() {
+
+    var pounds = parseInt(document.getElementById("weight").value);
+    var feets = parseInt(document.getElementById("height1").value);
+    var inches = parseInt(document.getElementById("height2").value);
+    var bmi = document.getElementById("bmi");
+
+    var pToKg = pounds * 0.45;                  // pound to Kg.
+    var heightTemp = (feets*12) + inches;       // convert and add feets & inches.
+    var heightToM = heightTemp * 0.025;         // convert feets to meters.
+    var sHeight = Math.pow(heightToM,2);        // square of the height.
+    var answer = pToKg / sHeight;               // weight/height.
+
+    bmi.innerHTML = answer.toFixed(1);
+}
